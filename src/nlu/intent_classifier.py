5f531f25e4bb0bcc7af11e24dbeaf4ccc232c88e
Picooooo <19520084@gm.uit.edu.vn>
2021-07-14 16:48:43 +0700

    new clf model

--- src/nlu/intent_classifier.py
+++ src/nlu/intent_classifier.py
@@ -2,6 +2,16 @@
 
 bad_chars = [';', ':', '!', "*", "-",".",",","%","(",")","/","{","}","[","]"]
 bad_words = ['nhé', 'nha', 'nhe', 'ha', 'nhen', 'he', 'hen']
+
+def softmax(Z):
+    e_Z = np.exp(Z)
+    A = e_Z / e_Z.sum(axis = 0)
+    return A
+
+def pred(W, X):
+    A = softmax(W.T.dot(X))
+    return np.argmax(A, axis = 0)
+
 class IntentClassifier:
     
     def predict_intent(self, sentence, pos_tag, ner_tag):
@@ -19,13 +29,20 @@ class IntentClassifier:
         
         # Template: always return class 0.
         vec = np.array(self.predict_1sen(sentence))
-        y_pred = self.model.predict([vec])
-        return y_pred[0]
+        #y_pred = self.model.predict([vec])
+        #return y_pred[0]
 
+        test_array = np.array([vec])
+        temp = test_array.T
+        K = np.concatenate((np.ones((1, len(test_array))), temp), axis = 0)
+        T = pred(self.model, K)
+        return T[0]
+        
     def load_intent_model(self):
         w2vmodel = "src/nlu/wiki.vi.model.bin" #dowload here https://thiaisotajppub.s3-ap-northeast-1.amazonaws.com/publicfiles/wiki.vi.model.bin.gz
         self.w2v_model = KeyedVectors.load_word2vec_format(w2vmodel, binary=True)
-        self.model = pickle.load(open('/CS221.L21.KHTN/src/nlu/model/clf_model/final_model.sav','rb'))
+        #self.model = pickle.load(open('/CS221.L21.KHTN/src/nlu/model/clf_model/final_model.sav','rb'))
+        self.model = pickle.load(open('/CS221.L21.KHTN/src/nlu/model/clf_model/final_model_test.sav','rb'))
     def predict_1sen(self, sen):
         sentence = [word for word in sen.lower().split()]
         print('Sentences:', sentence)
